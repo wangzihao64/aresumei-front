0227d02b29e2806c9326bae179f1d18bb8f80e44
wangzihao64 <185892713@qq.com>
2026-06-06 22:12:43 +0800

    界面配置

--- login/src/pages/register/index.jsx
+++ login/src/pages/register/index.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
 
 export default class Register extends Component {
-  render() {
-    return (
-      <div>Register</div>
-    )
-  }
+    render() {
+        return (
+            <form>
+                <div className="form-group">
+                    <label htmlFor="username">UserName</label>
+                    <input type="text" className="form-control" id="username" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" className="form-control" id="password" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="passwordConfirm">Confirm Password</label>
+                    <input type="password" className="form-control" id="passwordConfirm" />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        )
+    }
 }

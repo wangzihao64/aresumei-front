fb2bd312362e4043b8ae4fc4bf39fd1d10c34092
wangzihao64 <185892713@qq.com>
2026-06-12 00:03:18 +0800

    用户注册功能完成

--- login/src/pages/register/index.jsx
+++ login/src/pages/register/index.jsx
@@ -4,36 +4,70 @@ import {bindActionCreators} from 'redux';
 import {actionCreators as registerActionsCreators} from './store';
 class Register extends Component {
     state = {
-        username: '',
-        password: '',
-        passwordConfirm: ''
+        userInfo: {
+            username: '',
+            password: '',
+        },
+        errMsg: {
+            username: '',
+            password: '',
+        },
     }
-    handleSubmit = e=>{
+    handleSubmit = async e => {
         e.preventDefault();
-        // this.props.registerFn.registerAc();
-        console.log(this.props.registerData.name);
+        this.setState({ errMsg: { username: '', password: '' } });
+        const { data } = await this.props.registerFn.registerAc(this.state.userInfo);
+        // console.log(data);
+        // console.log(this.props.registerData.name);
+        const err = data.error || data.message || data.err || '';
+        const isUserExists = /用户已经存在|已存在/.test(err);
+        if (isUserExists) {
+            window.alert(err);
+            return;
+        }
+        if (data.status !== 200) {
+            const message = err || '注册失败，请检查输入。';
+            this.setState({
+                errMsg: {
+                    username: /用户名|用户/.test(message) && !/已存在/.test(message) ? message : '',
+                    password: /密码/.test(message) ? message : '',
+                }
+            });
+        } else {
+            this.setState({ errMsg: { username: '', password: '' } });
+        }
     }
-    handleChange = e=>{
+    handleChange = e => {
         this.setState({
-            [e.target.name]: e.target.value
+            userInfo: {
+                ...this.state.userInfo,
+                [e.target.name]: e.target.value,
+            }
         })
     }
     render() {
-        const { username, password, passwordConfirm } = this.state;
+        const { username, password } = this.state.userInfo;
+        const { errMsg } = this.state;
         return (
             <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="username">UserName</label>
                     <input type="text" className="form-control" name="username" id="username" defaultValue={username} onChange={this.handleChange} />
                 </div>
+                <small className="form-text text-danger">
+                    {errMsg.username}
+                </small>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
                     <input type="password" className="form-control" name="password" id="password" defaultValue={password} onChange={this.handleChange} />
                 </div>
-                <div className="form-group">
+                <small className="form-text text-danger">
+                    {errMsg.password}
+                </small>
+                {/* <div className="form-group">
                     <label htmlFor="passwordConfirm">Confirm Password</label>
                     <input type="password" className="form-control" name="passwordConfirm" id="passwordConfirm" defaultValue={passwordConfirm} onChange={this.handleChange} />
-                </div>
+                </div> */}
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         )
--- login/src/pages/register/store/actionCreator.js
+++ login/src/pages/register/store/actionCreator.js
@@ -1,4 +1,22 @@
-export const registerAc = () =>{
-    console.log('发起请求');
-    return {type:'register'};
+import axios from '../../../utils/request';
+
+const generateKey = length => {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let key = '';
+    for (let i = 0; i < length; i++) {
+        key += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return key;
+}
+
+export const registerAc = (data) =>{
+    const payload = {
+        user_name: data.username,
+        password: data.password,
+        key: generateKey(16)
+    };
+
+    return dispatch =>{
+        return axios.post('/api/v1/user/register', payload);
+    }
 }
